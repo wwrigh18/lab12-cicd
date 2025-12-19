8da80a2ddd221d2083a6f7399d8c5ad76c5a0b58
William Wright <162603673+wwrigh18@users.noreply.github.com>
2025-12-19 04:46:31 +0000

    Added a preview endpoint for the genz anonymizer

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -95,6 +95,17 @@ class Server:
         def deanonymizers():
             """Return a list of supported deanonymizers."""
             return jsonify(self.deanonymize.get_deanonymizers())
+        
+        # custom for lab
+        @self.app.route("/genz-preview", methods=["GET"])
+        def genz_preview():
+            """Return example Gen-Z anonymization output to make the feature tangible"""
+            content = {
+                      "example": "Call Emily at 577-988-1234",
+                      "example output": "Call GOAT at vibe check",
+                      "description": "Example output of the genz anonymizer."
+                    }
+            return jsonify(content)
 
         @self.app.errorhandler(InvalidParamError)
         def invalid_param(err):
